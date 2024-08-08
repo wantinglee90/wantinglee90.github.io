56e7df7f8e623053f64fa0f6fdef556a05e66e81
wantinglee90 <schwazle@gmail.com>
2024-08-08 16:46:39 -0700

    start

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.tsx',
+
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    port: 8080,
+    proxy: [{
+      context: ['/api'],
+      target: 'http://localhost:3000',
+    }],
+    historyApiFallback: true,
+    static: '/build',
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.scss'],
+  },
+  stats: {
+    children: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react', { targets: 'defaults' }],
+              ['@babel/preset-typescript']
+            ],
+          },
+        },
+      },
+      {
+        test: /(\.css|\.scss)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+  ],
+};
+
+module.exports = config;
