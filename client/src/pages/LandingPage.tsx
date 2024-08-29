81787f03fab99c7b1ee32b0f094d7e232d0c6765
wantinglee90 <schwazle@gmail.com>
2024-08-28 22:56:07 -0400

    ready to deploy

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -5,15 +5,19 @@ import TechStack from '../components/TechStack';
 import LandingSectionOne from '../components/LandingSectionOne';
 import LandingSectionTwo from '../components/LandingSectionTwo';
 import testsudo from '../assets/images/Testsudo.png';
-// import SectionOnePic2 from '../assets/images/SectionOnePic2.jpg';
-// import SectionTwoPic1 from '../assets/images/SectionTwoPic1.jpg';
-// import SectionTwoPic2 from '../assets/images/SectionTwoPic2.jpg';
-// import SectionThreePic1 from '../assets/images/SectionThreePic1.jpg';
-// import SectionThreePic2 from '../assets/images/SectionThreePic2.jpg';
+import occasio from '../assets/images/Occasio.png';
+import taxfolio from '../assets/images/Taxfolio.png';
+import voyager from '../assets/images/Voyager.png';
 
 const LandingPage: React.FC = () => {
 
-  const testsudoTech = ['NodeJS', 'ExpressJS', 'React', 'HTML/CSS', 'PostgreSQL', 'MongoDB', 'Docker', 'AWS', 'recharts'];
+  const testsudoTech = ['NodeJS', 'ExpressJS', 'React', 'Docker', 'AWS', 'Material-UI', 'recharts', 'OAuth', 'PostgreSQL', 'MongoDB'];
+
+  const occasioTech = ['OpenAI GPT-4o', 'NodeJS', 'ExpressJS', 'React', 'Jest', 'Figma', 'PostgreSQL', 'MongoDB'];
+
+  const taxfolioTech = ['NodeJS', 'React', 'Redux Toolkit Query', 'D3', 'Plaid API', 'Material-UI', 'PostgreSQL', 'MongoDB'];
+
+  const voyagerTech = ['NodeJS', 'ExpressJS', 'React', 'AWS S3', 'HTML/CSS', 'PostgreSQL', 'webpack'];
 
   return (
     <div className="container">
@@ -25,54 +29,67 @@ const LandingPage: React.FC = () => {
         leftOverlayColor='#6FC3DF'
         rightOverlayColor=''
         leftTitle='Testsudo'
-        leftSubtext='An open source observability tool to test frontend, backend and security performance'
+        leftSubtext='An open source observability tool for frontend, backend and security performance'
         rightTitle=''
         rightSubtext=''
         technology={testsudoTech}
       />
         <LandingSectionTwo
-        leftImage=''
-        rightImage='https://images.unsplash.com/photo-1474905187624-b3deaf7aa4c2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        leftImage={occasio}
+        rightImage='https://images.unsplash.com/photo-1415025148099-17fe74102b28?q=80&w=2736&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         leftOverlayColor=''
         rightOverlayColor='#8D82C4'
         leftTitle=''
         leftSubtext=''
         rightTitle='Occasio'
         rightSubtext='An AI-powered event planning tool'
-        technology={testsudoTech}
+        technology={occasioTech}
        />
-      {/* <LandingSectionOne 
-        leftImage='https://images.unsplash.com/photo-1421435371524-d26441ec7dda?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        rightImage='https://images.unsplash.com/photo-1415025148099-17fe74102b28?q=80&w=2736&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        leftOverlayColor='#6FC3DF'
-        rightOverlayColor='#8D82C4'
-        leftTitle='Testsudo'
-        leftSubtext='An open source observability tool to test frontend, backend and security performance'
-        rightTitle='Occasio'
-        rightSubtext='An AI-powered event planning tool'
-      /> */}
-      {/* <LandingSectionTwo
+      <LandingSectionOne 
         leftImage='https://images.unsplash.com/photo-1514575111941-6ce0097e6dfd?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        rightImage='https://images.unsplash.com/photo-1474905187624-b3deaf7aa4c2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        rightImage={taxfolio}
         leftOverlayColor='#EC8D81'
-        rightOverlayColor='#E7B788'
+        rightOverlayColor='4'
         leftTitle='TaxFolio'
         leftSubtext='An open source tax liabilities estimator'
+        rightTitle=''
+        rightSubtext=''
+        technology={taxfolioTech}
+      />
+        <LandingSectionTwo
+        leftImage={voyager}
+        rightImage='https://images.unsplash.com/photo-1474905187624-b3deaf7aa4c2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3'
+        leftOverlayColor=''
+        rightOverlayColor='#E7B788'
+        leftTitle=''
+        leftSubtext=''
         rightTitle='Voyager'
-        rightSubtext='A content and collaboration organizer'
-       /> */}
-       <LandingSectionOne 
+        rightSubtext='A collaboration tool for content creators'
+        technology={voyagerTech}
+       />
+       {/* <LandingSectionOne 
         leftImage='https://images.unsplash.com/photo-1468476396571-4d6f2a427ee7?q=80&w=2779&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        rightImage='https://images.unsplash.com/reserve/m6rT4MYFQ7CT8j9m2AEC_JakeGivens%20-%20Sunset%20in%20the%20Park.JPG?q=80&w=2310&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        rightImage=''
         leftOverlayColor='#8EA9E8'  
         rightOverlayColor='#87C5A4'
         leftTitle='TaxFolio'
         leftSubtext='An open source tax liabilities estimator'
         rightTitle=''
         rightSubtext=''
-        technology={testsudoTech}
+        technology={['']}
       />
-      <div className="footer" />
+        <LandingSectionTwo
+        leftImage=''
+        rightImage='https://images.unsplash.com/reserve/m6rT4MYFQ7CT8j9m2AEC_JakeGivens%20-%20Sunset%20in%20the%20Park.JPG?q=80&w=2310&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        leftOverlayColor=''
+        rightOverlayColor='#87C5A4'
+        leftTitle=''
+        leftSubtext=''
+        rightTitle='Voyager'
+        rightSubtext='A collaboration tool for content creators'
+        technology={['']}
+       /> */}
+      {/* <div className="footer" /> */}
     </div> 
   )
 }
