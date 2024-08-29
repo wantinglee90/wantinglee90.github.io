33b7bb4dcb34235bb8d4f46ad41699417c36d5f1
wantinglee90 <schwazle@gmail.com>
2024-08-28 21:29:21 -0400

    added screenshots

--- client/src/components/LandingSectionOne.tsx
+++ client/src/components/LandingSectionOne.tsx
@@ -17,14 +17,25 @@ const LandingSectionOne: React.FC<LandingPageProps> = ({leftImage, rightImage, l
         </div>
       </div> 
       <div className="section1-rightcontainer">
-        <img src={rightImage} className="background" />
-        <div className="section1-2-overlay" style ={{backgroundColor: rightOverlayColor}}>
+        <img src={rightImage} className="screenshot" />
+        <div className="techlist">
+          <div className="technology">NodeJS</div>
+          <div className="technology">ExpressJS</div>
+          <div className="technology">React</div>
+          <div className="technology">HTML/CSS</div>
+          <div className="technology">PostgreSQL</div>
+          <div className="technology">MongoDB</div>
+          <div className="technology">Docker</div>
+          <div className="technology">AWS</div>
+          <div className="technology">recharts</div>
+        </div>
+        {/* <div className="section1-2-overlay" style ={{backgroundColor: rightOverlayColor}}>
           <div className='section-text-container'>
             <div className="section-title"> {rightTitle} </div>
             <div className="line" />
             <div className="section-subtext"> {rightSubtext} </div>
           </div>
-        </div>
+        </div> */}
       </div> 
     </div>
   )
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -4,7 +4,7 @@ import LandingBanner from '../components/LandingBanner';
 import TechStack from '../components/TechStack';
 import LandingSectionOne from '../components/LandingSectionOne';
 import LandingSectionTwo from '../components/LandingSectionTwo';
-// import SectionOnePic1 from '../assets/images/SectionOnePic1.jpg';
+import testsudo from '../assets/images/Testsudo.png';
 // import SectionOnePic2 from '../assets/images/SectionOnePic2.jpg';
 // import SectionTwoPic1 from '../assets/images/SectionTwoPic1.jpg';
 // import SectionTwoPic2 from '../assets/images/SectionTwoPic2.jpg';
@@ -19,6 +19,26 @@ const LandingPage: React.FC = () => {
       <TechStack />
       <LandingSectionOne 
         leftImage='https://images.unsplash.com/photo-1421435371524-d26441ec7dda?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        rightImage={testsudo}
+        leftOverlayColor='#6FC3DF'
+        rightOverlayColor=''
+        leftTitle='Testsudo'
+        leftSubtext='An open source observability tool to test frontend, backend and security performance'
+        rightTitle=''
+        rightSubtext=''
+      />
+        <LandingSectionTwo
+        leftImage=''
+        rightImage='https://images.unsplash.com/photo-1474905187624-b3deaf7aa4c2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        leftOverlayColor=''
+        rightOverlayColor='#8D82C4'
+        leftTitle=''
+        leftSubtext=''
+        rightTitle='Occasio'
+        rightSubtext='An AI-powered event planning tool'
+       />
+      {/* <LandingSectionOne 
+        leftImage='https://images.unsplash.com/photo-1421435371524-d26441ec7dda?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         rightImage='https://images.unsplash.com/photo-1415025148099-17fe74102b28?q=80&w=2736&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         leftOverlayColor='#6FC3DF'
         rightOverlayColor='#8D82C4'
@@ -26,8 +46,8 @@ const LandingPage: React.FC = () => {
         leftSubtext='An open source observability tool to test frontend, backend and security performance'
         rightTitle='Occasio'
         rightSubtext='An AI-powered event planning tool'
-      />
-      <LandingSectionTwo
+      /> */}
+      {/* <LandingSectionTwo
         leftImage='https://images.unsplash.com/photo-1514575111941-6ce0097e6dfd?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         rightImage='https://images.unsplash.com/photo-1474905187624-b3deaf7aa4c2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         leftOverlayColor='#EC8D81'
@@ -36,16 +56,16 @@ const LandingPage: React.FC = () => {
         leftSubtext='An open source tax liabilities estimator'
         rightTitle='Voyager'
         rightSubtext='A content and collaboration organizer'
-       />
+       /> */}
        <LandingSectionOne 
         leftImage='https://images.unsplash.com/photo-1468476396571-4d6f2a427ee7?q=80&w=2779&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         rightImage='https://images.unsplash.com/reserve/m6rT4MYFQ7CT8j9m2AEC_JakeGivens%20-%20Sunset%20in%20the%20Park.JPG?q=80&w=2310&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        leftOverlayColor='#8EA9E8'
+        leftOverlayColor='#8EA9E8'  
         rightOverlayColor='#87C5A4'
-        leftTitle='Testsudo'
-        leftSubtext='An open source observability tool to test frontend, backend and security performance'
-        rightTitle='Occasio'
-        rightSubtext='An AI-powered event planning tool'
+        leftTitle='TaxFolio'
+        leftSubtext='An open source tax liabilities estimator'
+        rightTitle=''
+        rightSubtext=''
       />
       <div className="footer" />
     </div> 
