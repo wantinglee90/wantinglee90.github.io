bc157a6b2cbf50e32b51f5629c477613ebcd02d0
wantinglee90 <schwazle@gmail.com>
2024-09-04 14:08:51 -0400

    deploy

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import LandingPage from './pages/LandingPage';
+
+const App: React.FC = () => {
+
+  return (
+    <div>
+      <Header />
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App;
